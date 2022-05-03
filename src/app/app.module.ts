89c9b15d574a2bbc94ee8e99c46ce094d4bf31ff
Tejashwini R <Tejashwini.R@BRILLIO.COM>
2022-05-03 15:52:07 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
-import { AppComponent } from './app.component';
+ import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FormsModule } from '@angular/forms';
+
+import { FormControl,FormGroup,Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+   AppComponent,
+  LoginComponent,
+  RegisterComponent
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    FormGroup,
+    FormControl,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/title.pipe.ts
@@ -0,0 +1,13 @@
+import {Pipe,PipeTransform}from '@angular/core';
+@Pipe({
+  name: 'title'
+})
+export class TitlePipe implements PipeTransform{
+  transform(name:string,gender: string):string {
+    if(gender == 'male')
+    return 'Mr.'+name;
+    else
+    return 'Mrs.'+name;
+
+  }
+}
